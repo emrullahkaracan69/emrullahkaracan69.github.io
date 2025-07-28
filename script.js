5e8f4399f8bc1385952f307f21cdeb7cc4e2686e
emrullahkaracan69 <emrullah.karacan33@gmail.com>
2025-07-28 12:33:55 +0300

    background color and memoji changes

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 // Sayfa yüklendiğinde çalışacak kod
-document.addEventListener('DOMContentLoaded', function() {
+
+//document.addEventListener('DOMContentLoaded', function() {
 
     // HTML'deki memoji id'li resmi seç
-    const memoji = document.getElementById('memoji');
+  //  const memoji = document.getElementById('memoji');
 
     // Resimlerin yollarını değişkenlere ata
-    const defaultMemojiSrc = 'assets/memoji-default.png';
-    const hoverMemojiSrc = 'assets/memoji-hover.png';
+   // const defaultMemojiSrc = 'assets/memoji-default.png';
+  //  const hoverMemojiSrc = 'assets/memoji-hover.png';
 
     // Fare resmin üzerine geldiğinde...
-    memoji.addEventListener('mouseover', function() {
+   // memoji.addEventListener('mouseover', function() {
         // Resmin kaynağını (src) gülen memoji ile değiştir
-        memoji.src = hoverMemojiSrc;
-    });
+  //      memoji.src = hoverMemojiSrc;
+   // });
 
     // Fare resmin üzerinden çekildiğinde...
-    memoji.addEventListener('mouseout', function() {
+  //  memoji.addEventListener('mouseout', function() {
         // Resmin kaynağını (src) normal memoji ile değiştir
-        memoji.src = defaultMemojiSrc;
-    });
+  //      memoji.src = defaultMemojiSrc;
+  //  });
 
-});
+//});
